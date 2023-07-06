604d98d4beacd40b28c9e0cef438adcb02922b38
glori76 <119129837+glori76@users.noreply.github.com>
2023-07-06 23:44:32 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,105 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
+// ...
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+
+Route::resource('employees', EmployeeController::class);
+Route::post('/employees/upload', [EmployeeController::class, 'uploadFile'])->name('employees.upload');
+
+Auth::routes();
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+
+Route::get('/local-disk', function() {
+    Storage::disk('local')->put('local-disksaya.txt', 'ini adalah contoh dari local disk punya glory');
+    return asset('storage/local-example.txt');
+    });
+
+Route::get('/public-disk', function() {
+     Storage::disk('public')->put('public-example.txt', 'This is public example content');
+      return asset('storage/public-example.txt');
+    });
+
+    Route::get('/retrieve-local-file', function() {
+        if (Storage::disk('local')->exists('local-example.txt')) {
+            $contents = Storage::disk('local')->get('local-example.txt');
+        } else {
+            $contents = 'File does not exist';
+        }
+
+        return $contents;
+    });
+
+    Route::get('/retrieve-public-file', function() {
+        if (Storage::disk('public')->exists('public-example.txt')) {
+            $contents = Storage::disk('public')->get('public-example.txt');
+        } else {
+            $contents = 'File does not exist';
+        }
+
+        return $contents;
+    });
+
+    Route::get('/download-local-file', function() {
+        return Storage::download('local-example.txt', 'local file');
+    });
+
+    Route::get('/download-public-file', function() {
+        return Storage::download('public/public-example.txt', 'public file');
+    });
+
+    Route::get('/file-url', function() {
+        // Just prepend "/storage" to the given path and return a relative URL
+        $url = Storage::url('local-example.txt');
+        return $url;
+    });
+
+    Route::get('/file-size', function() {
+        $size = Storage::size('local-example.txt');
+        return $size;
+    });
+
+    Route::get('/file-path', function() {
+        $path = Storage::path('local-example.txt');
+        return $path;
+    });
+
+    Route::get('/upload-example', function() {
+        return view('upload_example');
+    });
+
+    Route::post('/upload-example', function(Request $request) {
+        $path = $request->file('avatar')->store('public');
+        return $path;
+    })->name('upload-example');
+
+    Route::get('/delete-local-file', function(Request $request) {
+        Storage::disk('local')->delete('local-example.txt');
+        return 'Deleted';
+    });
+
+    Route::get('/delete-public-file', function(Request $request) {
+        Storage::disk('public')->delete('public-example.txt');
+        return 'Deleted';
+    });
+
+    Route::get('download-file/{employeeId}', [EmployeeController::class, 'downloadFile'])->name('employees.downloadFile');
